762cb7be21e2ea489042055d97c4358c8753d0b0
Lyzh <haylyzh@gmail.com>
2018-03-14 12:05:08 +0800

    a commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import createReactClass from 'create-react-class';
+
+/** I will create a pull request with this commit and I will edit line 5 later on new branch */
