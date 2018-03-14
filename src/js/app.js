1a39b6f7b88f4d764fa1949743aa18268e53c513
Lyzh <haylyzh@gmail.com>
2018-03-14 12:45:30 +0800

    test2
    # Conflicts:
    #	src/js/app.js

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,8 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import createReactClass from 'create-react-class';
 
+<<<<<<< HEAD
 /** I will create a pull request with this commit and I will edit line 5 later on new branch */
+=======
+/** This is another branch and it's expected that there will be merge conflict after test 1 branch is merged to amster */
+>>>>>>> 34618c4... test2
